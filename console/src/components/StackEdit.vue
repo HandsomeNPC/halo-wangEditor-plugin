84ba1e861ff698ffb084b8e287c2e4542b01069c
chenziwen <401030526@qq.com>
2023-04-27 16:17:32 +0800

    1.可以将编辑内容保存到数据库了

--- console/src/components/StackEdit.vue
+++ console/src/components/StackEdit.vue
@@ -16,50 +16,48 @@
   </div>
 </template>
 
-<script>
+<script lang="ts" setup>
 import '@wangeditor/editor/dist/css/style.css' // 引入 css
 
-import {onBeforeUnmount, ref, shallowRef, onMounted} from 'vue'
+import {onBeforeUnmount, ref, shallowRef, onMounted, watch} from 'vue'
 import {Editor, Toolbar} from '@wangeditor/editor-for-vue'
 
-export default {
-  components: {Editor, Toolbar},
-  setup() {
-    // 编辑器实例，必须用 shallowRef
-    const editorRef = shallowRef()
-
-    // 内容 HTML
-    const valueHtml = ref('')
-
-    // 模拟 ajax 异步获取内容
-    onMounted(() => {
-      setTimeout(() => {
-        valueHtml.value = ''
-      }, 1500)
-    })
-
-    const toolbarConfig = {}
-    const editorConfig = {placeholder: '请输入内容...'}
-
-    // 组件销毁时，也及时销毁编辑器
-    onBeforeUnmount(() => {
-      const editor = editorRef.value
-      if (editor == null) return
-      editor.destroy()
-    })
-
-    const handleCreated = (editor) => {
-      editorRef.value = editor // 记录 editor 实例，重要！
-    }
-
-    return {
-      editorRef,
-      valueHtml,
-      mode: 'default', // 或 'simple'
-      toolbarConfig,
-      editorConfig,
-      handleCreated
-    };
-  }
+// 编辑器实例，必须用 shallowRef
+const editorRef = shallowRef();
+//模式:默认
+const mode = 'default';
+// 内容 HTML
+const valueHtml = ref('');
+// 模拟 ajax 异步获取内容
+onMounted(() => {
+  setTimeout(() => {
+    valueHtml.value = ''
+  }, 1500)
+})
+//工具栏配置
+const toolbarConfig = {}
+//编辑框配置
+const editorConfig = {placeholder: '请输入内容...'}
+// 组件销毁时，也及时销毁编辑器
+onBeforeUnmount(() => {
+  const editor = editorRef.value
+  if (editor == null) return
+  editor.destroy()
+})
+const handleCreated = (editor: any) => {
+  editorRef.value = editor // 记录 editor 实例，重要！
 }
+const emit = defineEmits<{
+  (event: "update:raw", value: string): void;
+  (event: "update:content", value: string): void;
+}>();
+
+watch(
+  () => valueHtml.value,
+  () => {
+    emit("update:raw", valueHtml.value);
+    emit("update:content", valueHtml.value);
+  }
+);
+
 </script>   
